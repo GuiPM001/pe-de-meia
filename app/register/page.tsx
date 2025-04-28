9a01169735e3cf82505cc0f4a5df372778af8f80
GuiPM001 <guilhermepimentelm0@gmail.com>
2025-04-28 20:14:50 -0300

    register page

---
+++ app/register/page.tsx
@@ -0,0 +1,73 @@
+"use client";
+
+import React, { useState } from "react";
+import logo from "@/app/assets/logo.png";
+import Image from "next/image";
+import Input from "../components/core/input";
+import PasswordInput from "../components/core/passwordInput";
+import Button from "../components/core/button";
+
+export default function Register() {
+  const [form, setForm] = useState({
+    name: "",
+    email: "",
+    password: "",
+  });
+
+  const handleForm = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setForm({
+      ...form,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const submitRegister = () => {
+    console.log(form);
+  };
+
+  return (
+    <div className="flex h-screen bg-gradient-to-br from-green-100 to-primary">
+      <div className="flex flex-col w-1/2 bg-white items-center justify-center p-8">
+        <Image alt="Pé de meia logo" src={logo} width={384} height={188} />
+        <p className="text-gray-600 mt-4 max-w-72 text-wrap text-center">
+          Enxergue seu futuro financeiro com clareza e segurança.
+        </p>
+      </div>
+
+      <div className="flex w-1/2 items-center justify-center p-8">
+        <div className="bg-white rounded-2xl shadow-2xl p-10 w-full max-w-md">
+          <h2 className="text-2xl font-bold text-primary-dark text-center mb-8">
+            Criar uma conta
+          </h2>
+
+          <div className="space-y-6">
+            <Input
+              label="Nome"
+              placeholder="Seu nome"
+              name="name"
+              value={form.name}
+              onChange={handleForm}
+            />
+            <Input
+              label="E-mail"
+              placeholder="seu@email.com"
+              name="email"
+              value={form.email}
+              onChange={handleForm}
+            />
+            <PasswordInput
+              placeholder="********"
+              name="password"
+              value={form.password}
+              onChange={handleForm}
+            />
+
+            <Button type="submit" onClick={submitRegister}>
+              Cadastrar
+            </Button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
