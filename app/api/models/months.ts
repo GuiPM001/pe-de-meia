5ac7ec7fd31adf67bb284b37c0811335754657f5
João Vitor Clavilho <jv.clavilho@hotmail.com>
2025-04-28 21:19:18 -0300

    Validações para cadastro de mes

--- app/api/models/months.ts
+++ app/api/models/months.ts
@@ -2,8 +2,8 @@ import mongoose, { model, models } from 'mongoose';
 
 const MonthSchema = new mongoose.Schema({
   monthBalance: { type: Number, required: true },
-  isUser: { type: String, required: true },
-  idMounth: { type: String, required: true },
+  idUser: { type: String, required: true },
+  idMonth: { type: Date, required: true },
   createdAt: { type: Date, default: Date.now },
 });
 
