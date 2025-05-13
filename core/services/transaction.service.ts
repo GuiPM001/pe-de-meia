baad49ab02f3ee390ffd4387926a7d6950589b52
João Vitor Clavilho <jv.clavilho@hotmail.com>
2025-05-13 17:26:21 -0300

    add support for registering recurring transactions

--- core/services/transaction.service.ts
+++ core/services/transaction.service.ts
@@ -2,6 +2,7 @@ import { connectMongo } from "@/core/db/mongodb";
 import { Transactions } from "../models/transactions";
 import { Transaction } from "../types/Transaction";
 import { monthService } from "./month.service";
+import { User } from "../models/user";
 
 const registerTransaction = async (transaction: Transaction) => {
   const { date, value, description, type, idUser, idMonth } = transaction;
@@ -13,8 +14,36 @@ const registerTransaction = async (transaction: Transaction) => {
 
   await connectMongo();
 
-  await Transactions.create(transaction);
-  
+  const user = await User.findById({ _id: idUser });
+
+  if (!user)
+    throw new Error("É necessário um usuário cadastrado cadastrar um mês");
+
+  switch (transaction.recurrent) {
+    case true:
+      const originalDate = new Date(transaction.date);
+      const month = originalDate.getUTCMonth();
+
+      const transactionsToInsert = [];
+
+      for (let i = 0; i < 12; i++) {
+        const futureDate = new Date(originalDate);
+        futureDate.setMonth(month + i);
+        transactionsToInsert.push({
+          ...transaction,
+          date: futureDate.toISOString(),
+          recurrent: true,
+        });
+      }
+
+      await Transactions.insertMany(transactionsToInsert);
+      break;
+
+    case false:
+      await Transactions.create(transaction);
+      break;
+  }
+
   const months = await monthService.getFutureMonthsByIdUser(idUser, idMonth);
 
   await months.forEach(async (month) => {
