369a515b4a8af0d9a6cabe7b4499795bb01ef6c6
GuiPM001 <guilhermepimentelm0@gmail.com>
2025-05-20 11:51:02 -0300

    add metadata in head html

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,13 @@
 import { ProfileProvider } from "./context/ProfileContext";
 import "./globals.css";
+
+export const metadata = {
+  title: "Pé de Meia",
+  icons: {
+    icon: "/favicon.ico",
+  },
+};
+
 export default function RootLayout({
   children,
 }: Readonly<{
@@ -7,6 +15,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="pt">
+      <head></head>
       <body className="font-nunito">
         <ProfileProvider>{children}</ProfileProvider>
       </body>
