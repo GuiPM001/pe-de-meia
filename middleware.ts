745d7e21e0a4a1ccefb2602203a848b68120ca3c
GuiPM001 <guilhermepimentelm0@gmail.com>
2025-11-05 12:41:20 -0300

    fix apple-touch-icon

--- middleware.ts
+++ middleware.ts
@@ -71,6 +71,6 @@ export async function middleware(request: NextRequest) {
 
 export const config = {
   matcher: [
-    "/((?!api|_next/static|_next/image|favicon.ico|sitemap.xml|robots.txt).*)",
+    "/((?!api|_next/static|_next/image|favicon.ico|apple-touch-icon.png|sitemap.xml|robots.txt).*)",
   ],
 };
