9e83bd42f6db905310d2ad7a44167237cad226d5
GuiPM001 <guilhermepimentelm0@gmail.com>
2025-11-13 23:53:03 -0300

    fix build

--- components/calendar/useCalendar.ts
+++ components/calendar/useCalendar.ts
@@ -100,7 +100,6 @@ export const useCalendar = ({ month, indexMonth, year }: CalendarProps) => {
       investeds: null,
       total: null,
       totalInvested: null,
-      description: "",
       ...values,
     });
   };
