5815875394d68b1b472ef702519e05c9e98c5f53
Guilherme Pimentel <guilhermepimentelm0@gmail.com>
2026-01-08 22:11:50 -0300

    fix date to isoString

--- core/utils/date.extensions.ts
+++ core/utils/date.extensions.ts
@@ -7,5 +7,9 @@ declare global {
 }
 
 Date.prototype.toISODateString = function (): string {
-  return this.toISOString().split("T")[0];
+  const year = this.getFullYear();
+  const month = String(this.getMonth() + 1).padStart(2, "0");
+  const day = String(this.getDate()).padStart(2, "0");
+
+  return `${year}-${month}-${day}`;
 };
